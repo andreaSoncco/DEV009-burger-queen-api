d228c95f7618673c22f1650e661db46a64212d3f
andreaSoncco <andrea.soncco.c@gmail.com>
2023-10-13 17:59:57 -0500

    Hacer los primeros cambios para el uso de MongoDB en el proyecto

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
+const { MongoClient } = require('mongodb');
+const config = require("./config");
+
+const client = new MongoClient(config.dbUrl);
+
+async function connect() {
+  try {
+    await client.connect();
+    const db = client.db(BurgerQueenAPI);
+    return db;
+  } catch (error) {
+    //
+  }
+}
+
+/*
 const express = require('express');
-const config = require('./config');
 const authMiddleware = require('./middleware/auth');
 const errorHandler = require('./middleware/error');
 const routes = require('./routes');
@@ -28,3 +43,30 @@ routes(app, (err) => {
     console.info(`App listening on port ${port}`);
   });
 });
+*/
+
+
+/* const { MongoClient } = require("mongodb");
+
+// Replace the uri string with your connection string.
+const uri = "mongodb+srv://daleskaSoncco:tomaleche88.@data.qyk0xrv.mongodb.net/?retryWrites=true&w=majority";
+
+const project = new MongoClient(uri);
+
+async function run() {
+  try {
+    const database = project.db('BurgerQueenAPI');
+    const users = database.collection('Users');
+
+    // Query for a movie that has the title 'Back to the Future'
+    const query = { role: 'admin' };
+    const user = await users.findOne(query);
+
+    console.log(user);
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await project.close();
+  }
+}
+run().catch(console.dir);
+*/
