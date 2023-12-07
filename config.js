08669c13bc18d7b4509abeda9536c7422528f4c4
andreaSoncco <andrea.soncco.c@gmail.com>
2023-12-07 12:07:20 -0500

    Asociarlo a la base de datos mongodb para el despliegue

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 exports.port = process.argv[2] || process.env.PORT || 8080;
-exports.dbUrl = process.env.MONGO_URL || process.env.DB_URL || 'mongodb+srv://daleskaSoncco:tomaleche88.@data.qyk0xrv.mongodb.net/?retryWrites=true&w=majority';
+exports.dbUrl = process.env.MONGO_URL || process.env.DB_URL || 'mongodb+srv://andreasoncco:FihyQUvO6h4NTRny@mongodb.g4jsjsj.mongodb.net/mongodb?retryWrites=true&w=majority';
 exports.secret = process.env.JWT_SECRET || 'esta-es-la-api-burger-queen';
 exports.adminEmail = process.env.ADMIN_EMAIL || 'andrea.soncco.c@gmail.com';
 exports.adminPassword = process.env.ADMIN_PASSWORD || 'ElijoSerFeliz27';
